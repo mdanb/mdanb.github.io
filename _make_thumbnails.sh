010d8180cb5db246949ee9e422ed6999a705de0e
mdanb <mdanb@umich.edu>
2024-07-08 11:21:39 -0400

    f

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,9 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+for file in images/*.png; do
+	[ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
 done
+
+for file in images/*.jpeg; do
+        echo $file
+        [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+done
+
